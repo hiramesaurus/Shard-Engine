70782db6b05956262cd2a707a1f980a5508529b2
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2018-12-28 09:42:35 +0200

    And moved the vector to own file!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+mod vector;
+
 fn main() {
-    let vec = Vector3{
+    let vec = vector::Vector3 {
         x: 4.0,
         y: 5.0,
         z: 2.0,
@@ -7,37 +9,6 @@ fn main() {
 
     let norm = vec.normalized ();
 
-    println!("{:?}", vec);
-    println!("{:?}", norm);
-}
-
-#[derive(Debug)]
-struct Vector3  {
-    x : f32,
-    y : f32,
-    z : f32,
-}
-
-impl Vector3 {
-    fn cross (&self, other: Vector3) -> Vector3 {
-        Vector3 {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0
-        }
-    }
-
-    fn normalized (&self){
-        let mag = self.magnitude();
-        Vector3{
-            x: self.x / mag,
-            y: self.y / mag,
-            z: self.z / mag,
-        };
-    }
-
-    fn magnitude (&self) -> f32 {
-        let sqr_mag = self.x * self.x + self.y * self.y + self.z + self.z;
-        sqr_mag.sqrt()
-    }
+    println!("Vector {:?}", vec);
+    println!("Normalized {:?}", norm);
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,14 +1,12 @@
-#[derive(debug)]
-struct Vector3  {
-    x : f32,
-    y : f32,
-    z : f32,
+#[derive(Debug)]
+pub struct Vector3  {
+    pub x : f32,
+    pub y : f32,
+    pub z : f32,
 }
 
-use std::num;
-
 impl Vector3 {
-    fn cross (&self, other: Vector3) -> Vector3 {
+    pub fn cross (&self, other: Vector3) -> Vector3 {
         Vector3 {
             x: 0.0,
             y: 0.0,
@@ -16,17 +14,17 @@ impl Vector3 {
         }
     }
 
-    fn normalized (&self){
+    pub fn normalized (&self) -> Vector3 {
         let mag = self.magnitude();
         Vector3{
             x: self.x / mag,
             y: self.y / mag,
             z: self.z / mag,
-        };
+        }
     }
 
-    fn magnitude (&self) -> f64 {
+    pub fn magnitude (&self) -> f32 {
         let sqr_mag = self.x * self.x + self.y * self.y + self.z + self.z;
-        num::sqrt (sqr_mag)
+        sqr_mag.sqrt()
     }
 }
