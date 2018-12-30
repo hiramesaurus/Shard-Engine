ba165de5d99127fa5ed79274ee7b8e6034fadadc
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2018-12-30 12:24:38 +0200

    fixed cross product
    also added ´return´ to all functions

--- src/vector.rs
+++ src/vector.rs
@@ -7,20 +7,20 @@ pub struct Vector3  {
 
 impl Vector3 {
     pub fn cross (&self, other: Vector3) -> Vector3 {
-        Vector3 {
-            x: self.y * other.z - self.z * other.c,
+        return Vector3 {
+            x: self.y * other.z - self.z * other.y,
             y: self.z * other.x - self.x * other.z,
             z: self.y * other.y - self.y * other.x
         }
     }
 
-    pub fn Dot (&self, other: Vector3) -> f32 {
+    pub fn dot (&self, other: Vector3) -> f32 {
         return self.x * other.x + self.y * other.y + self.z * other.z;
     }
 
     pub fn normalized (&self) -> Vector3 {
         let mag = self.magnitude();
-        Vector3{
+        return Vector3 {
             x: self.x / mag,
             y: self.y / mag,
             z: self.z / mag,
@@ -29,6 +29,6 @@ impl Vector3 {
 
     pub fn magnitude (&self) -> f32 {
         let sqr_mag = self.x * self.x + self.y * self.y + self.z + self.z;
-        sqr_mag.sqrt()
+        return sqr_mag.sqrt()
     }
 }
